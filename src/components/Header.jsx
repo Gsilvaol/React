9d892f66fc89cb95b92527c8e1562959152bb4d3
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-02-02 14:48:53 -0300

    componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
-import styles from'./Header.module.css'
+import styles from './Header.module.css'
+
+import igniteLogo from '../assets/Ignite-logo.svg';
 export function Header() {
     return (
-        <strong className={styles.header}>Ignite Feed</strong>
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Logotipo do Ignite" />
+        </header>
     );
 }
