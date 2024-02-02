be71ded92969a66fb88d87703b6a5c9c844c7554
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-02-02 16:24:13 -0300

    componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
+
 export function App() {
   return (
     <div>
       <Header />
-      
-      <Post author="Gustavo" content="Vamos a luta"/>
-      <Post author="Rafael" content="Eu também estou muito animado" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Gustavo" content="Vamos a luta" />
+          <Post author="Rafael" content="Eu também estou muito animado" />
+        </main>
+
+      </div>
     </div>
   )
 }
