fb412285ec8cebb442b2e20404b3a61c64cc9b5a
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-02-02 11:07:02 -0300

    entendendo css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './styles.css';
 export function App() {
   return (
     <div>
+      <Header />
+      
       <Post author="Gustavo" content="Vamos a luta"/>
       <Post author="Rafael" content="Eu também estou muito animado" />
     </div>
