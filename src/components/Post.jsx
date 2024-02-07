d182565d476dcff7f00e8d8f44e74c054ff161a8
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-02-07 15:35:31 -0300

    componente post

---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/Gsilvaol.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Gustavo Silva de oliveira</strong>
+                        <span>Análise e Desenvolvimento de sistemas</span>
+                    </div>
+                </div>
+
+                <time title="07 de fevereiro ás 15:30h" dateTime="2024-02-07 15:29:00">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉<a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+    )
+}
