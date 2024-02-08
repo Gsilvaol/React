c63dcf9db78e7903e0c3c4691373b39f55459594
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-02-08 15:13:23 -0300

    componente avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/Gsilvaol.png" />
+                    <Avatar src="https://github.com/Gsilvaol.png" />
                     <div className={styles.authorInfo}>
                         <strong>Gustavo Silva de oliveira</strong>
                         <span>Análise e Desenvolvimento de sistemas</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
+
 
 export function Sidebar() {
     return (
@@ -8,8 +10,8 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1699864312705-069068c1e78e?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/Gsilvaol.png" />
-
+                <Avatar src="https://github.com/maykbrito.png" />
+                
                 <strong>Gustavo Silva de Oliveira</strong>
                 <span>Estudante de Tecnologia</span>
             </div>
